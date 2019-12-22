4be226d7c6d483f20a66412a8ebbf9ca180854c1
zhanshenzhi2008 <lj2010vip@163.com>
2019-12-22 20:07:22 +0800

    zkClient

---
+++ src/main/java/com/orjrs/zk/client/jdkapi/ZkSession.java
@@ -0,0 +1,49 @@
+package com.orjrs.zk.client.jdkapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.*;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * 创建zK会话
+ *
+ * @author orjrs
+ * @create 2019-12-22 15:35
+ * @since 1.0.0
+ */
+@Slf4j
+public class ZkSession {
+    /** ZK服务端地址 */
+    private static final String SERVER_ADDR = "192.168.135.132";
+
+    /** ZK会话超时时间 */
+    private static final int SESSION_TIMEOUT = 200;
+
+    /**
+     * 获得session的方式，这种方式可能会在ZooKeeper还没有获得连接的时候就已经对ZK进行访问了
+     */
+    @Test
+    public void createSession() {
+        try {
+
+            // 这里watcher不能为空
+            // ZooKeeper zooKeeper = new ZooKeeper(SERVER_ADDR, SESSION_TIMEOUT, null);
+            ZooKeeper zooKeeper = new ZooKeeper(SERVER_ADDR, SESSION_TIMEOUT, event -> {
+                log.info("zookeeper");
+                System.out.println(String.format("event! type=%s, stat=%s, path=%s", event.getType(),
+                        event.getState(), event.getPath()));
+            });
+            zooKeeper.create("/orjrs1", "smart".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            String zNode = "/orjrs1";
+            log.info("zNode={}", new String(zooKeeper.getData(zNode, false, null)));
+        } catch (KeeperException e) {
+            log.info("创建ZK session KeeperException： {}", e.getMessage());
+        } catch (InterruptedException e) {
+            log.info("创建ZK session InterruptedException中断： {}", e.getMessage());
+        } catch (IOException e) {
+            log.info("创建ZK session IOException： {}", e.getMessage());
+        }
+    }
+}
